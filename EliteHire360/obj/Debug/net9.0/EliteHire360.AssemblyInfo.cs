fec1efb2485ddf00a40ce063b76116fb7a29db21
Mathan <captainamericain1918@gmail.com>
2025-10-15 18:35:01 +0530

    main commit

--- EliteHire360/obj/Debug/net9.0/EliteHire360.AssemblyInfo.cs
+++ EliteHire360/obj/Debug/net9.0/EliteHire360.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EliteHire360")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebbd65068879a64c34b8726874b62185beb7b10f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69402c93a94854cbf7af80f15bcba13022526f1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EliteHire360")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EliteHire360")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
